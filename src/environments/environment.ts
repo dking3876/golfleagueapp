634a21dc978f8d02101a2e8c6f74cccf825fbab4
Deryk w. King <dking3876@msn.com>
2023-05-04 18:43:47 -0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'golfleague-afb49',
+    appId: '1:343629968830:web:c1962833ef85b6d9eaf5ef',
+    storageBucket: 'golfleague-afb49.appspot.com',
+    apiKey: 'AIzaSyAUTJQrNMjFVcn_KZxsli_7nYi7bmuc-7U',
+    authDomain: 'golfleague-afb49.firebaseapp.com',
+    messagingSenderId: '343629968830',
+  },
   production: false
 };
 
